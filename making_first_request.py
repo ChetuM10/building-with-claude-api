fdc4550e2f113a27c61657ddc2bea98d317e4e04
natehc <chetanchetu4940@gmail.com>
2026-05-11 14:07:03 +0530

    update:Initiated Multi-Turn Conversation

--- making_first_request.py
+++ making_first_request.py
@@ -6,17 +6,76 @@ load_dotenv()
 api_key = os.getenv("ANTHROPIC_API_KEY")
 
 client = Anthropic(api_key=api_key)
-model = "claude-sonnet-4-6"
+model = "claude-sonnet-4-5"
 
-response = client.messages.create(
-    model = model,
-    max_tokens = 1000,
-    messages = [
-        {
-            "role": "user",
-            "content": "Who is Naruto Uzumaki? Answer in one sentence."
-        }
-    ]
-)
+def add_user_message(messages, text):
+    user_message = { "role": "user", "content": text}
+    messages.append(user_message)
 
-print(response.model_dump_json(indent=2))
+def add_assistant_message(messages, text):
+    assistant_message = { "role": "assistant", "content": text}
+    messages.append(assistant_message)
+
+
+def chat(messages):
+    message = client.messages.create(
+        model = model,
+        max_tokens = 1000,
+        messages = messages
+    )
+    return message.content[0].text
+
+
+
+# Make a starting list of messages
+messages = []
+
+# Add in the initial user question 
+add_user_message(messages, "Tell me about Naruto Uzumaki in one sentence.")
+
+# Pass the list of messages into 'chat' to get an answer
+answer = chat(messages)
+print("\n Answer 1:", answer)
+
+
+# ------------------------------------------------------------------#
+
+
+# Take the answer and add it as an assistant message into our list
+add_assistant_message(messages, answer)
+
+# Add in the user's follow-up question
+add_user_message(messages, "List me top 3 quotes from Naruto, including one of his own.")
+
+# Call chat again with the list of messages to get a final answer
+answer = chat(messages)
+
+#Final answer
+print("\n Answer 2:", answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(response)
+# print(response.content[0].text)
+
+# # Add this to see immediate usage data
+# print(f"\nUsage: {response.usage}")
